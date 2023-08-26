1b1a6271b8e975f2e020aa636d1f41f40c2ac809
yangpci <yangpci@163.com>
2023-08-27 01:47:04 +0800

    update: base02

--- main.go
+++ main.go
@@ -7,27 +7,35 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/hello", helloHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	engine := new(Engine)
+	log.Fatal(http.ListenAndServe(":8080", engine))
 }
-func indexHandler(resp http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(resp, "URL.Path = %q\n", req.URL.Path)
-}
-func helloHandler(resp http.ResponseWriter, req *http.Request) {
-	for k, v := range req.Header {
-		fmt.Fprintf(resp, "Header[%q] = %q\n", k, v)
+
+type Engine struct{}
+
+func (engine *Engine) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	switch req.URL.Path {
+	case "/":
+		fmt.Fprintf(resp, "URL.Path = %q\n", req.URL.Path)
+	case "/hello":
+		for k, v := range req.Header {
+			fmt.Fprintf(resp, "Header[%q] = %q\n", k, v)
+		}
+	default:
+		fmt.Fprintf(resp, "404 NOT FOUND: %s\n", req.URL)
 	}
 }
 
 /*
-1. GET 127.0.0.1:8080/
+1. GET http://127.0.0.1:8080/
 	URL.Path = "/"
 
-2. GET 127.0.0.1:8080/hello
+2. GET http://127.0.0.1:8080/hello
 	Header["Accept"] = ["* /*"]
-	Header["Postman-Token"] = ["c2709648-34a9-4bc3-b012-1fb8b97e55bf"]
+	Header["Postman-Token"] = ["34ed4dd7-4ba8-48c5-aed2-748504ceb54a"]
 	Header["Accept-Encoding"] = ["gzip, deflate, br"]
 	Header["Connection"] = ["keep-alive"]
-	Header["User-Agent"] = ["PostmanRuntime/7.32.3"]
+
+3. GET http://127.0.0.1:8080/world
+	404 NOT FOUND: /world
 */
