cfe353b4e3d353637eeb0f07ae417ebd2e8c8715
yangpci <yangpci@163.com>
2023-08-27 02:03:25 +0800

    update: base01

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/hello", helloHandler)
+	http.ListenAndServe(":8080", nil)
+}
+func indexHandler(resp http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(resp, "URL.Path = %q", req.URL.Path)
 }
 
-
+func helloHandler(resp http.ResponseWriter, req *http.Request) {
+	for k, v := range req.Header {
+		fmt.Fprintf(resp, "Header[%q] = %q\n", k, v)
+	}
+}
 
 /*
 1. GET http://127.0.0.1:8080/
@@ -16,7 +29,4 @@ func main() {
 	Header["Postman-Token"] = ["34ed4dd7-4ba8-48c5-aed2-748504ceb54a"]
 	Header["Accept-Encoding"] = ["gzip, deflate, br"]
 	Header["Connection"] = ["keep-alive"]
-
-3. GET http://127.0.0.1:8080/world
-	404 NOT FOUND: /world
 */
