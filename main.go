7e55382793ffe872081ec263cc329deeaa856f24
Yangpc <129414398+yangpsychologists@users.noreply.github.com>
2023-08-27 01:54:04 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,30 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
 
 func main() {
-	engine := new(Engine)
-	log.Fatal(http.ListenAndServe(":8080", engine))
+
 }
 
-type Engine struct{}
 
-func (engine *Engine) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
-	switch req.URL.Path {
-	case "/":
-		fmt.Fprintf(resp, "URL.Path = %q\n", req.URL.Path)
-	case "/hello":
-		for k, v := range req.Header {
-			fmt.Fprintf(resp, "Header[%q] = %q\n", k, v)
-		}
-	default:
-		fmt.Fprintf(resp, "404 NOT FOUND: %s\n", req.URL)
-	}
-}
 
 /*
 1. GET http://127.0.0.1:8080/
